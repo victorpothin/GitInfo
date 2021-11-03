c210952133d435d0abc7fc16e8b98fb1f8c6d157
Victor Pothin <victorpothin1@gmail.com>
2021-11-03 10:56:25 -0300

    add controller

--- src/GitInfo.Web/Controllers/LogController.cs
+++ src/GitInfo.Web/Controllers/LogController.cs
@@ -1,20 +1,20 @@
 using GitInfo.Domain.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-// namespace GitInfo.Web.Controllers
-// {
-//     [ApiController]
-//     [Route("Log")]
-//     public class LogController : ControllerBase
-//     {
-//         public LogController()
-//         {
-//         }
+namespace GitInfo.Web.Controllers
+{
+    [ApiController]
+    [Route("log")]
+    public class LogController : ControllerBase
+    {
+        public LogController()
+        {
+        }
 
-//         [HttpGet]
-//         public ActionResult Get()
-//         {
-//             return Ok("ok");
-//         }
-//     }
-// }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
